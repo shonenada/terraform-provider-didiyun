9f7f0f97e1e98733c1e955f525e6cedcb82a525f
shonenada <shonenada@gmail.com>
2021-01-08 17:07:36 +0800

    feat: add filter in dc_images example

--- examples/dc2_images/main.tf
+++ examples/dc2_images/main.tf
@@ -2,5 +2,13 @@ provider "didiyun" {}
 
 data "didiyun_dc2_images" "dc2_images" {
   region_id = var.region_id
+
+  filter {
+    img_type = "standard"
+    os_family = "Ubuntu"
+    os_version = "18.04"
+    platform = "Linux"
+    scene = "base"
+  }
 }
 
